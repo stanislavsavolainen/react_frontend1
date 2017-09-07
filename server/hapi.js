6d79cd62d194784e5614d5d59ae58d34f4cecdb6
stanislav.savolainen <stanislav.savolainen@mail.com>
2017-09-07 13:59:09 +0300

    add date to backend console log, test 1

--- server/hapi.js
+++ server/hapi.js
@@ -20,6 +20,47 @@ const joi_object1 = { payload: { check: Joi.string().min(3).max(30).allow().rege
 const joi_object2 = {};
 const joi_object3 = {};
 
+
+function timestamp_function() {
+
+    /*
+    let timestamp = new Date().getTime();
+
+    let timestamp_hours = new Date().getHours();
+    let timestamp_minutes = new Date().getMinutes();
+    let timestamp_seconds = new Date().getSeconds();
+
+    let timestamp_day = new Date().getDay();
+    let timestamp_month = new Date().getMonth();
+    let timestamp_year = new Date().getFullYear();
+
+    //6.9.2017,10:43:20
+    let timestamp_string = timestamp_day + '.' + timestamp_month + '.' + timestamp_year  + ',' + timestamp_hours + ':' + timestamp_minutes + ':' + timestamp_seconds ;
+    
+    let timestamp_string = [];
+    timestamp_string.splice(0, timestamp_string.length);
+    
+    timestamp_string.push(timestamp_day);
+    timestamp_string.push('.');
+    timestamp_string.push(timestamp_month);
+    timestamp_string.push('.');
+    timestamp_string.push(timestamp_year);
+  //  timestamp_string.push(',');
+    timestamp_string.push(timestamp_hours);
+    timestamp_string.push(':');
+    timestamp_string.push(timestamp_minutes);
+    timestamp_string.push(':');
+    timestamp_string.push(timestamp_seconds);
+*/
+   
+    //return ' -> timestamp ' + timestamp_string;
+
+    var server_date_and_time = new Date().toString();
+
+    return ' -> timestamp ' + server_date_and_time ;
+}
+
+
 // Add the route
 server.route([
 
@@ -30,20 +71,20 @@ server.route([
         handler: function (request, reply) {
 
             let x = request.payload.check;
+            //let timestamp = new Date().getTime();
 
-
-            console.log("Book handler : " + x);
+            console.log("Book handler, message -> " + x + timestamp_function());
 
             //return reply('hello world');
-            return reply(JSON.stringify({ body: 'buying book' }));
+            return reply(JSON.stringify({ body: 'buying book'  + timestamp_function() }));
         },
         //config : { valuez : Joi.string().min(5).max(20) } //validate string between 5 and 20 characters
 
-        /*    
+
         config: {
             validate: joi_object1,
         },
-        */
+
     },
     //------------------- route 2 -----------------------
     {
@@ -51,10 +92,12 @@ server.route([
         path: '/car',
         handler: function (request, reply) {
             let x = request.payload.check;
-            console.log("Book handler : " + x);
-            return reply(JSON.stringify({ body: 'buying car !' }));
+            //let timestamp = new Date().getTime();
+
+            console.log("Car handler, message -> " + x + timestamp_function());
+            return reply(JSON.stringify({ body: 'buying car' + timestamp_function() }));
         },
-       // config: {}
+        // config: {}
     },
     //------------------- route 3 -----------------------
     {
@@ -62,10 +105,11 @@ server.route([
         path: '/dvd',
         handler: function (request, reply) {
             let x = request.payload.check;
-            console.log("Book handler : " + x);
-            return reply(JSON.stringify({ body: 'trying to buy dvd' }));
+           // let timestamp = new Date().getTime();
+            console.log("DVD handler, message -> " + x + timestamp_function());
+            return reply(JSON.stringify({ body: 'trying to buy dvd' + timestamp_function() }));
         },
-       // config: {}
+        // config: {}
     },
 
 
